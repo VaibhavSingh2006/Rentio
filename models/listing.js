73e22b77bac83f4b416120696cf66caf39ee54d2
Vaibhav Singh <vaibhavrjp8112@gmail.com>
2025-12-26 13:52:20 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  description: String,
+
+  image: {
+      type: String,
+      default:
+        "https://www.luxuryvillastay.com/wp-content/uploads/2023/05/luxury-villa.jpg",
+  },
+
+  price: Number,
+  location: String,
+  country: String,
+});
+
+module.exports = mongoose.model("Listing", listingSchema);
